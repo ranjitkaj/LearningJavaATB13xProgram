a9675504b14c58f9b391131325a9cb05b31b587a
Ranjit Kumar <ranjitkajraitha@gmail.com>
2025-06-16 11:24:28 +0530

    New commit

---
+++ src/ex_01_Java_Basic/ex_01_Java_Basic/Ranjit1.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class Ranjit1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ex_01_Java_Basic/ex_01_Java_Basic/Ranjit_ji.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class Ranjit_ji {
+    public static void main(String[] args) {
+        System.out.println("Hello, Ranjit ji!");
+    }
+}
