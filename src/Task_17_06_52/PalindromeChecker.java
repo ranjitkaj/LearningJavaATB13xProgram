0c0febbc4fd111c1a77f154dd659585f0e8c1eed
Ranjit Kumar <ranjitkajraitha@gmail.com>
2025-06-17 12:27:35 +0530

    hii

---
+++ src/Task_17_06_52/PalindromeChecker.java
@@ -0,0 +1,27 @@
+package Task_17_06_52;
+
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = scanner.nextLine();
+
+        boolean isPalindrome = true;
+        int len = str.length();
+
+        for (int i = 0; i < len / 2; i++) {
+            if (str.charAt(i) != str.charAt(len - i - 1)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(str + " is a Palindrome.");
+        } else {
+            System.out.println(str + " is NOT a Palindrome.");
+        }
+    }
+}
